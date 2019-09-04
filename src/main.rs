89125022ae33b6c3f8a11f0ca803f0d5e3112407
Aron <arostr-5@student.ltu.se>
2019-09-04 19:13:38 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,49 @@
 extern crate nom;
+use nom::{
+  IResult,
+  bytes::complete::{tag, take_while_m_n},
+  combinator::map_res,
+  sequence::tuple};
 
-use nom::sequence::tuple;
-use nom::{IResult,
-character::complete::{alpha1, digit1}
-};
+use nom::{combinator::iterator, character::complete::alpha1, sequence::terminated};
 
 use std::str::FromStr;
-use crate::Expr::{Root, Value};
+
+use crate::Tree::{Operand, Value};
 
 
 #[derive(Debug)]
-enum Expr {
-    Root(char, Box<Expr>, Box<Expr>),
+enum Tree {
+    Operand(char, Box<Tree>, Box<Tree>),
     Value(i32),
     Nil,
 }
 
+fn is_char_digit(c: char) -> bool {
+    c.is_digit(32)
+}
+
+fn build_tree(c: char, tree: Option<Box<Tree>>) -> IResult<&str, Box<Tree>> {
+
+    Tree::Value(c.parse::<i32>().unwrap())
+
+}
 
 
 
-fn parse(input: &str) -> IResult<&str, &str> {
+fn parse(input: &str) -> Tree {
+
+
+    let mut tree: Option<Box<Tree>> = None;
+    for c in input.chars() {
+        if is_char_digit(c) {
+            build_tree(c, tree).unwrap();
+        }
+    }
+    tree
 
     
-    // i32::from_str(nom::character::complete::digit1(input).unwrap()).unwrap();
-    digit1(input)
+
 }
 
 
