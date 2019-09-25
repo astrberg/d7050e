047e7387c8e0dfba8b9ba298a6d302f03f09238f
Aron Strandberg <arostr-5@student.ltu.se>
2019-09-25 11:31:44 +0200

    update

--- compiler/src/interpreter.rs
+++ compiler/src/interpreter.rs
@@ -16,11 +16,11 @@ pub fn statement(s: &Statement) -> HashMap<String, Value> {
     let mut instructions = HashMap::new(); 
     match s {
         
-        Statement::Let(var, _typ, op, expr) => {
+        Statement::Let(var, _typ, op, exp) => {
             let var = unbox(var.clone()).into();
             match op {
                 Op::Equal => {
-                    instructions.insert(var, Value::Int(bin_expr(&expr)));
+                    instructions.insert(var, Value::Int(expr(&exp, instructions)));
                 },
                     
                 // Op::AddEq
@@ -36,13 +36,16 @@ pub fn statement(s: &Statement) -> HashMap<String, Value> {
     
 }
 
-pub fn bin_expr(e: &Expr) -> i32 {
-
+pub fn expr(e: &Expr, instructions: HashMap<String, Value>) -> i32 {
     match e {
+        Expr::Var(i) => match instructions.get(&*i) {
+            Some(Value::Int(v)) => *v,
+            _ => panic!(),
+        }
         Expr::Number(i) => *i,
         Expr::Op(l, op, r) => {
-            let l = bin_expr(&l);
-            let r = bin_expr(&r);
+            let l = expr(&l,instructions);
+            let r = expr(&r,instructions);
             match op {
                 Op::Add => l + r, 
                 Op::Sub => l - r,
